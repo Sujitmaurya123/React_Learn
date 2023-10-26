aded7036cadd624a974f5acc5573e96deb3d115f
Sujitmaurya123 <sujitkic6802maurya@gmail.com>
2023-10-26 17:13:53 +0530

    make CurrencyChange App

---
+++ currencyConvertor/src/hooks/useCurrencyInfo.js
@@ -0,0 +1,16 @@
+ import { useEffect,useState } from "react";
+
+
+ function useCurrencyInfo(currency){
+    const [data,setData]=useState({})
+     let url = `https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/${currency}.json`
+
+    useEffect(()=>{
+        fetch(url).then((res)=>res.json())
+        .then((res)=>setData(res[currency]))
+        // console.log(data);
+    },[currency])
+    //  console.log(data);
+    return data;
+ }
+ export default useCurrencyInfo;
